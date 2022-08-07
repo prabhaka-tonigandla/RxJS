9efb10b1f020fb9075045ee69cfb40a62c32059e
prabhaka-tonigandla <prabhakar.tonigandla@icloud.com>
2022-08-07 18:28:53 +0530

    map,tap and take operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { from, map, Observable, observable, of } from 'rxjs';
+import { from, map, Observable, observable, of, tap } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -8,12 +9,28 @@ import { from, map, Observable, observable, of } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 ngOnInit(): void {
- of(2,4,6).pipe(
-   map(item=> item *4)
+ of(40,30,20,80).pipe(
+   tap(item=> console.log(`emitted item..${item}`)),
+   map(item=> item *2),
+   tap(item=> console.log(`emitted item..${item}`)),
+   map(item=> item-20),
+   map(item=>
+    {
+      if(item===20)
+      {
+        throw new Error('Zero detected')
+      }
+      else
+      {
+        return item;
+      }
+    }),
+    take(1)
  ).subscribe(
-   item=>console.log(item)
-  
-   
+   {
+  next:item=>console.log(item),
+  error: err => console.error(`error detected : ${err}`)
+   }
    )
 }
 
