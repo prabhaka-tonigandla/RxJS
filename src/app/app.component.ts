0b0efa69c8a5a932d23bc25b7fdf300ecbe6ff82
prabhaka-tonigandla <prabhakar.tonigandla@icloud.com>
2022-08-07 14:38:32 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+ngOnInit(): void {
+  const Observer=
+  {
+   next:item => console.log(`emitted item from next: ${item}`),
+   error:err => console.log(`emitted item from error: ${err}`),
+   complete:()=> console.log(`completed`),
+  };
+  const apples$ = new Observable(appleSubscriber =>{
+    appleSubscriber.next("Apple1");
+    appleSubscriber.next("Apple1");
+    appleSubscriber.next("Apple3");
+    appleSubscriber.complete();
+
+  });
+
+  const sub = apples$.subscribe(Observer);
+  
+}
+
+}
