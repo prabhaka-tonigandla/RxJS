dbcaefe3c0f2aff28e48add7069006ec777f06b8
prabhaka-tonigandla <prabhakar.tonigandla@icloud.com>
2022-08-07 18:04:53 +0530

    from and of Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { Observable, observable } from 'rxjs';
+import { from, Observable, observable, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -8,26 +8,16 @@ import { Observable, observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 ngOnInit(): void {
-  const Observer=
-  {
-   next:item => console.log(`emitted item from next: ${item}`),
-   error:err => console.log(`emitted item from error: ${err}`),
-   complete:()=> console.log(`completed`),
-  };
-  const apples$ = new Observable(appleSubscriber =>{
-    appleSubscriber.next("Apple1");
-    appleSubscriber.next("Apple1");
-    appleSubscriber.next("Apple3");
-    appleSubscriber.complete();
-
-  });
-
-  const sub = apples$.subscribe( {
-    next:item => console.log(`emitted item from next: ${item}`),
-    error:err => console.log(`emitted item from error: ${err}`),
-    complete:()=> console.log(`completed`),
-   });
-  
+  of('Private Applications','ModGL Wires').subscribe(
+    {
+    next:item=> console.log(item)
+    }
+  );
+  from(['Private Applications','ModGL Wires']).subscribe(
+    {
+    next:item=> console.log(`from observable ${item}`)
+    }
+  );
 }
 
 }
